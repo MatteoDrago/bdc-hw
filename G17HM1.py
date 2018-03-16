1c0beca18818a2c80acecc4feb131adfcefaeb1b
DrMello <matteo.drago1994@gmail.com>
2018-03-16 15:50:50 +0100

    Tried to configure spark. Gives UnicodeEncodeError. WARNING is fine on Windows

--- G17HM1.py
+++ G17HM1.py
@@ -2,11 +2,22 @@ from pyspark import SparkContext, SparkConf
 
 # Import the Dataset
 lNumbers = []
+
 with open('dataset.txt', 'r') as f:
     lines = f.readlines()
     for n in lines:
     	if len(n[:-1]) > 0:
     		lNumbers.append(float(n[:-1]))
 
-
 print lNumbers
+
+# Spark Setup
+
+conf = SparkConf().setAppName('Preliminaries')
+sc = SparkContext(conf=conf)
+
+# Create a parallel collection
+dNumbers = sc.parallelize(lNumbers)
+
+sumOfSquares = dNumbers.map(lambda s: s^2).reduce(lambda a, b: a + b)
+#print "The sum of squares is" + sumOfSquares
