4f1560140cd9c93b2c203634d7c8dc84a36ca7ba
RicLincio <37301469+RicLincio@users.noreply.github.com>
2018-04-11 14:21:25 +0200

    Final version

--- Homework 2/G17HM2_python_3.py
+++ Homework 2/G17HM2_python_3.py
@@ -9,7 +9,7 @@ sc = SparkContext(conf=config)
 
 # Load the Dataset
 filename = sys.argv[-1]
-docs = sc.textFile(filename).cache()
+docs = sc.textFile(filename).cache() #Here we could have repartitioned the RDD but, since we are running the code with a modest input for our machine capabilities, we can avoid it.
 N_documents = docs.count()
 N_words = docs.flatMap(lambda document: document.split(' ')).count()
 
@@ -21,7 +21,7 @@ print()
 
 # Improved WordCount 1
 def f1(document) :
-    pairs_dict = {}
+    pairs_dict = {} # we used dictionary to store (key, value) pairs to use native data structure capabilities
     for word in document.split(' '):
         if word not in pairs_dict.keys():
             pairs_dict[word] = 1
@@ -40,6 +40,7 @@ wordcount_1 = docs.flatMap(f1) \
                 .groupByKey() \
                 .map(f2)
 
+# Time meausure struct (used also for the following methods)
 t0 = time.time()
 n_dif_words = wordcount_1.count()
 t1 = time.time()
@@ -59,7 +60,7 @@ def f3(document):
             dictionary[word] += 1
         else:
             dictionary[word] = 1
-    return [(np.random.randint(partitions), (k,dictionary[k])) for k in dictionary.keys()]
+    return [(np.random.randint(partitions), (k,dictionary[k])) for k in dictionary.keys()] # returns list of (x, (word, c_word))
     
 def f4(document):
     pairs_dict = {}
